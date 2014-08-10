3ecdc83752f654de170ffb71bd113124c23213e5
meni181818 <meni181818@gmail.com>
2014-08-10 17:02:18 +0300

    Update renameProperties.js

--- renameProperties.js
+++ renameProperties.js
@@ -1,37 +1,56 @@
 function renameProperties(sourceObj, replaceList, destObj) {
     destObj = destObj || {};
     // for each property in source object
-    for(var prop in sourceObj) {
+    $.each(sourceObj, function(key) {
         // if the property really exist
-        if(sourceObj.hasOwnProperty(prop)) {
+        if(sourceObj.hasOwnProperty(key)) {
             
-            // if the property is object
-            if(typeof sourceObj[prop] === 'object') {
+            // if the child key is array
+            if(sourceObj[key] instanceof Array) {
                 // if it in the replace List (as property)
-                if(replaceList[prop]) {
-                    var strName = replaceList[prop];
-                    destObj[strName] = {};
+                if(replaceList[key]) {
+                    var newName = replaceList[key];
+                    destObj[newName] = [];
                     // send it to replaceAttrNames() function (recursively)
-                    renameProperties(sourceObj[prop], replaceList, destObj[strName]);
+                    renameProperties(sourceObj[key], replaceList, destObj[newName]);
+                    
                 // if its NOT in the replace List (as property)
-                } else if(!replaceList[prop]) {
-                    destObj[prop] = {};
-                    renameProperties(sourceObj[prop], replaceList, destObj[prop]);
+                } else if(!replaceList[key]) {
+                    destObj[key] = [];
+                    renameProperties(sourceObj[key], replaceList, destObj[key]);
                 }
                 
-            // if the property is not object
-            } else if (typeof sourceObj[prop] != 'object') {
+            // if the child key is object
+            } else if(typeof sourceObj[key] === 'object') {
                 // if it in the replace List (as property)
-                if(replaceList[prop]) {
-                    var strName = replaceList[prop];
-                    destObj[strName] = sourceObj[prop];
+                if(replaceList[key]) {
+                    var newName = replaceList[key];
+                    // create new property in the destObj named as the new name
+                    destObj[newName] = {};
+                    // send it to replaceAttrNames() function (recursively)
+                    renameProperties(sourceObj[key], replaceList, destObj[newName]);
+                    
                 // if its NOT in the replace List (as property)
-                } else if(!replaceList[prop]) {
-                    destObj[prop] = sourceObj[prop];
+                } else if(!replaceList[key]) {
+                    destObj[key] = {};
+                    renameProperties(sourceObj[key], replaceList, destObj[key]);
+                }
+                
+            // if the child key is NOT object and NOT Array
+            } else {
+                // if it in the replace List (as property)
+                if(replaceList[key]) {
+                    var newName = replaceList[key];
+                    destObj[newName] = sourceObj[key];
+                // if its NOT in the replace List (as property)
+                } else if(!replaceList[key]) {
+                    destObj[key] = sourceObj[key];
                 }
             }
+            
         }
-    } 
+    });
+    
     return destObj;
 }
 
