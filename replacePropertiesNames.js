d626ba449ccc8cb2cc44a4f9a3702d52a3686bdb
meni181818 <meni181818@gmail.com>
2014-08-10 03:17:50 +0300

    Create replacePropertiesNames.js

---
+++ replacePropertiesNames.js
@@ -0,0 +1,36 @@
+function replaceAttrName(sourceObj, replaceList, destObj) {
+    destObj = destObj || {};
+    // for each property in source object
+    for(var prop in sourceObj) {
+        // if the property really exist
+        if(sourceObj.hasOwnProperty(prop)) {
+            
+            // if the property is object
+            if(typeof sourceObj[prop] === 'object') {
+                // if it in the replace List (as property)
+                if(replaceList[prop]) {
+                    var strName = replaceList[prop];
+                    destObj[strName] = {};
+                    // send it to cloner function (recursively)
+                    replaceAttrName(sourceObj[prop], replaceList, destObj[strName]);
+                // if its NOT in the replace List (as property)
+                } else if(!replaceList[prop]) {
+                    destObj[prop] = {};
+                    replaceAttrName(sourceObj[prop], replaceList, destObj[prop]);
+                }
+                
+            // if the property is not object
+            } else if (typeof sourceObj[prop] != 'object') {
+                // if it in the replace List (as property)
+                if(replaceList[prop]) {
+                    var strName = replaceList[prop];
+                    destObj[strName] = sourceObj[prop];
+                // if its NOT in the replace List (as property)
+                } else if(!replaceList[prop]) {
+                    destObj[prop] = sourceObj[prop];
+                }
+            }
+        }
+    } 
+    return destObj;
+}
